ce035aedf76757b1ca57d81610e00d99a53177fa
Yuya Ninomaru <yuya.ninomaru@gmail.com>
2020-01-14 09:15:57 +0900

    home.bladeに位置情報取得処理

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,10 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- API -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuKj5VOfkkixfBl_s0doXmcm74n4jzOag"></script>
+
+
     <!-- Styles -->
     <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <!-- <link href="{{ asset('scss/jTinder.scss') }}" rel="stylesheet"> -->
@@ -64,6 +68,43 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        window.onload = function() {
+            console.log('loaded');
+            // Google Map API 位置情報取得
+            //getMyPlace();
+        }
+
+        function getMyPlace() {
+            if (!navigator.geolocation) { //Geolocation apiがサポートされていない場合
+                alert("<p>Geolocationはあなたのブラウザーでサポートされておりません</p>");
+                return;
+            }
+            function success(position) {
+                var latitude = position.coords.latitude; //緯度
+                var longitude = position.coords.longitude; //経度
+                console.log(latitude, longitude);
+                // //output.innerHTML = '<p>緯度 ' + latitude + '° <br>経度 ' + longitude + '°</p>';
+                // // 位置情報
+                // var latlng = new google.maps.LatLng(latitude, longitude);
+                // // Google Mapsに書き出し
+                // var map = new google.maps.Map(document.getElementById('map'), {
+                //     zoom: 15, // ズーム値
+                //     center: latlng, // 中心座標
+                // });
+                // // マーカーの新規出力
+                // new google.maps.Marker({
+                //     map: map,
+                //     position: latlng,
+                // });
+            };
+            function error() {
+                //エラーの場合
+                alert("座標位置を取得できません");
+            };
+            navigator.geolocation.getCurrentPosition(success, error); //成功と失敗を判断
+        }
+    </script>
 
 </body>
 
