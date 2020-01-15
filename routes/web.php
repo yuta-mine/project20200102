84deb020f7881cddd8112876ffdc9e6792124fd3
shinohara-san <someiyoshino0809@gmail.com>
2020-01-15 17:29:56 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,13 @@ Route::group(['prefix' => 'users', 'middleware' => 'auth'], function () {
     // 下記のルートを追加 ユーザーのセッティング画面に遷移する maru
     Route::get('show/{id}/setting', 'UserController@setting')->name('users.setting');
     // 下記のルートを追加 チャットルームへ移動する suke
-    Route::get('chat/{id}',function(){
-        return view('chat');
-    });
+   
 
 });
+// まずchat/{id}にmatchtableのidをぶち込む。<=
+// そしてChatController@〇〇に送って、そのcontroller内の〇〇関数で、マッチテーブルidを基に引っ張ってきた2つのuseridのユーザーのみのメッセージが表示されるチャットルームにリダイレクトする。
+
+// Route::get('chat/{id}', ChatController@create);
 
 Auth::routes();
 
@@ -56,4 +58,3 @@ Route::get('/picture', 'Auth\RegisterController@picture')->name('picture');
 Route::post('/register', 'Auth\RegisterController@create')->name('registerend');
 
 // 会員登録時のページ遷移しながらデータ保持して次へ行く処理 ここまで shino
-
